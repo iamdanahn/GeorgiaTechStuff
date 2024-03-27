958f5bd318dfc5d0d4d69e97857e66905e3046a4
Daniel Ahn <iamdanahn@gmail.com>
2024-03-26 20:39:14 -0400

    add base switch statement

--- OOP - Java 1/HW02/Calculator.java
+++ OOP - Java 1/HW02/Calculator.java
@@ -9,17 +9,32 @@ public class Calculator {
 
     Set<String> validOperations = new HashSet<>(Set.of("add", "subtract", "multiply", "divide", "alphabetize"));
 
-    // Ask question
+    // #1 - Ask question
     System.out.println("List of operations: add subtract multiply divide alphabetize");
 
-    // Get and check if input operation is valid
+    // #2 - Get and check if input operation is valid
     System.out.println("Enter an operation: ");
     String operation = input.next();
     String lowerOperation = operation.toLowerCase();
     // exit if invalid
     if (!validOperations.contains(lowerOperation)) {
       exitLine();
+      System.exit(0);
     }
+
+    // #3 - perform chosen operation
+    switch (lowerOperation) {
+      case "add", "subtract":
+        break;
+      case "multiply", "divide":
+        break;
+      case "alphabetize":
+        break;
+      default:
+        break;
+
+    }
+
     
   }
   
