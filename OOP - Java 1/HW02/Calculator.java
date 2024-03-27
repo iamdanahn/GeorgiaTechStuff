9830dbf99ce7014ddfc883fe5a3367b4742c760c
Daniel Ahn <iamdanahn@gmail.com>
2024-03-26 20:20:42 -0400

    Create base class and add first print line

---
+++ OOP - Java 1/HW02/Calculator.java
@@ -0,0 +1,8 @@
+package HW02;
+import java.util.Scanner;
+
+public class Calculator {
+  public static void main(String[] args) {
+    System.out.println("List of operations: add subtract multiply divide alphabetize");
+  }
+}
