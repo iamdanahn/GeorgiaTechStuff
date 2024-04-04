0fe4dd717606c1b49159f1a7238e0c1c61a7c896
Daniel Ahn <iamdanahn@gmail.com>
2024-04-03 20:16:36 -0400

    build blank board for both players

--- OOP - Java 1/HW03/Battleship.java
+++ OOP - Java 1/HW03/Battleship.java
@@ -1,7 +1,24 @@
 import java.util.Scanner;
 public class Battleship {
 	public static void main(String[] args) {
+    // 1. Print out starting message
+		System.out.println("Welcome to Battleship!");
 
+		// 2. Prompt each user to enter coordinates for 5 ships
+		char[][] p1Board = new char[5][5];
+		char[][] p2Board = new char[5][5];
+		buildInitialBoard(p1Board);
+		buildInitialBoard(p2Board);
+
+	}
+
+	private static void buildInitialBoard(char[][] playerBoard) {
+		for (int row = 0; row < 5; row++) {
+			for (int column = 0; column < 5; column++) {
+				playerBoard[row][column] = '-';
+			}
+		}
+		printBattleShip(playerBoard);
 	}
 
 	// Use this method to print game boards to the console.
