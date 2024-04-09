8d6f78ae367da8ffcc6ebe08b0996e20ebb19faa
Daniel Ahn <iamdanahn@gmail.com>
2024-04-08 21:25:07 -0400

    random tests

---
+++ OOP - Java 1/random.java
@@ -0,0 +1,30 @@
+public class random {
+  public static void main(String[] args) {
+    System.out.println("Jackets".equals("Jackets"));
+  }
+  // public static void main(String[] args) {
+  //   int upNext = 1332;
+  //   switch (upNext) {
+  //     case 1:
+  //       upNext -= 1;
+  //     case 3:
+  //       upNext -= 2;
+  //     case 13:
+  //       upNext -= 3;
+  //     case 1332:
+  //       upNext++;
+  //     case 1333:
+  //       upNext--;
+  //   }
+  //   System.out.println(upNext);
+  // }
+
+  // public static void main(String[] args) {
+  //   String pos = "guard";
+  //   int val = (4 / 6) / 2 + pos.indexOf("pointguard");
+  //   double dub = (4 / 6) / 2 + pos.indexOf("pointguard");
+  //   System.out.println(val);
+  //   System.out.println(dub);
+  //   System.out.println(pos.indexOf("pointguard"));
+  // }
+}
