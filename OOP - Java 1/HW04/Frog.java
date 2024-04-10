3a83b9554d4a1be7a5cbe1e9ed35904d70e9be4a
Daniel Ahn <iamdanahn@gmail.com>
2024-04-10 16:19:17 -0400

    base instance variables

--- OOP - Java 1/HW04/Frog.java
+++ OOP - Java 1/HW04/Frog.java
@@ -1,6 +1,11 @@
 public class Frog {
+  private String name;
+  private int age;
+  private double tongueSpeed;
+  private boolean isFroglet; // froglets are > 1 month & < 7 months
   
-  
+  private static final String species = "Rare Pepe";
+
   
   public static void main(String[] args) {
   }
