e3c8d38c09f1f2f063b06951af0e66bec499ed03
Daniel Ahn <iamdanahn@gmail.com>
2024-04-10 16:23:06 -0400

    add constructors with chaining

--- OOP - Java 1/HW04/Frog.java
+++ OOP - Java 1/HW04/Frog.java
@@ -6,6 +6,20 @@ public class Frog {
   
   private static final String species = "Rare Pepe";
 
+
+  public Frog(String name) {
+    this(name, 0.4166, 5.0);
+  }
+
+  public Frog(String name, double ageInYears, double tongueSpeed) {
+    this(name, int(ageInYears), tongueSpeed);
+  }
+
+  public Frog(String name, int age, double tongueSpeed) {
+    this.name = name;
+    this.age = age;
+    this.tongueSpeed = tongueSpeed;
+  }
   
   public static void main(String[] args) {
   }
