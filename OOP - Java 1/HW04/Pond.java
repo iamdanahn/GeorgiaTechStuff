da2ffb42afeb8375ce7b7cbe1419263efe959bab
Daniel Ahn <iamdanahn@gmail.com>
2024-04-10 22:13:14 -0400

    - Print out on a new line the description of the Frog named Peepo given by the toString method.
    - Have the Frog named Peepo attempt to eat the Fly with a mass of 6.

--- OOP - Java 1/HW04/Pond.java
+++ OOP - Java 1/HW04/Pond.java
@@ -1,16 +1,21 @@
 public class Pond {
 
   public static void main(String[] args) {
-    Frog frog1 = new Frog('Peepo');
-    Frog frog2 = new Frog('Pepe', 10, 15);
-    Frog frog3 = new Frog('Peepaw', 4.6, 5);
-    Frog frog4 = new Frog('Lewis', 12, 100);
+    Frog peepo = new Frog('Peepo');
+    Frog pepe = new Frog('Pepe', 10, 15);
+    Frog peepaw = new Frog('Peepaw', 4.6, 5);
+    Frog lewis = new Frog('Lewis', 12, 100);
 
     Fly fly1 = new Fly(1, 3);
     Fly fly2 = new Fly(6);
     Fly fly3 = new Fly();
 
-    frog1.setSpecies('1331 Frogs');
+    peepo.setSpecies('1331 Frogs');
+
+    System.out.println(peepo.toString());
+    
+    peepo.eat(fly2);
+    
   }
 
 }
