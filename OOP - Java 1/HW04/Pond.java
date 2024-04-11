28975ce385df9c58db2963eb053b7d393e2a0c47
Daniel Ahn <iamdanahn@gmail.com>
2024-04-10 22:30:46 -0400

    java uses "" for strings and '' for chars

--- OOP - Java 1/HW04/Pond.java
+++ OOP - Java 1/HW04/Pond.java
@@ -1,17 +1,17 @@
 public class Pond {
 
   public static void main(String[] args) {
-    Frog peepo = new Frog('Peepo');
-    Frog pepe = new Frog('Pepe', 10, 15);
-    Frog peepaw = new Frog('Peepaw', 4.6, 5);
-    Frog lewis = new Frog('Lewis', 12, 100);
+    Frog peepo = new Frog("Peepo");
+    Frog pepe = new Frog("Pepe", 10, 15);
+    Frog peepaw = new Frog("Peepaw", 4.6, 5);
+    Frog lewis = new Frog("Lewis", 12, 100);
 
     Fly fly1 = new Fly(1, 3);
     Fly fly2 = new Fly(6);
     Fly fly3 = new Fly();
 
     // Set the species of any Frog to “1331 Frogs”
-    peepo.setSpecies('1331 Frogs');
+    peepo.setSpecies("1331 Frogs");
 
     // Print out on a new line the description of the Frog named Peepo given by the toString method.
     System.out.println(peepo.toString());
