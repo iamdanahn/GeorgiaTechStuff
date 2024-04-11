fdadbb99783fe9abbdf2e98a713d2461d13ee8ae
Daniel Ahn <iamdanahn@gmail.com>
2024-04-10 21:36:30 -0400

    add setters/getters for instance vars

--- OOP - Java 1/HW04/Frog.java
+++ OOP - Java 1/HW04/Frog.java
@@ -6,7 +6,6 @@ public class Frog {
   
   private static final String species = "Rare Pepe";
 
-
   public Frog(String name) {
     this(name, 0.4166, 5.0);
   }
@@ -20,7 +19,38 @@ public class Frog {
     this.age = age;
     this.tongueSpeed = tongueSpeed;
   }
+
+  public String getName() {
+    return this.name;
+  }
+  private String setName(String newName) {
+    return this.name = newName;
+  }
+  public int getAge() {
+    return this.age;
+  }
+  private int setAge(int newAge) {
+    return this.age = newAge;
+  }
+  public double getTongueSpeed() {
+    return this.tongueSpeed;
+  }
+  private double setTongueSpeed(double newTongueSpeed) {
+    return this.tongueSpeed = newTongueSpeed;
+  }
+  public boolean getIsFroglet() {
+    return this.isFroglet;
+  }
+  private boolean setIsFroglet(boolean newIsFroglet) {
+    return this.isFroglet = newIsFroglet;
+  }
   
   public static void main(String[] args) {
+  }
+
+  public 
+
+  private void grow(int months) {
+    
   }
 }
