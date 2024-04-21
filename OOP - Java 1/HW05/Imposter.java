3d5b0bab3b0413dbe9b29afab68a54172aac4a66
Daniel Ahn <iamdanahn@gmail.com>
2024-04-20 20:45:53 -0400

    This file defines an interface with the name Impostor. Impostors will be able to mess with Players through sabotage and freeze tagging them.

---
+++ OOP - Java 1/HW05/Imposter.java
@@ -0,0 +1,8 @@
+public interface Imposter {
+
+  // Abstract method that takes in a Player object and does not return anything
+  public void freeze(Player p);
+
+  // Abstract method that takes in a Player object and does not return anything
+  public void sabotage(Player p);
+}
