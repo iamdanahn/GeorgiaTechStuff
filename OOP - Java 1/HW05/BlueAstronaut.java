42b6f0c798cdabcbc310615eba1c74d1c4511cf0
Daniel Ahn <iamdanahn@gmail.com>
2024-04-22 20:29:34 -0400

    - numTasks - the number of tasks that needs to be completed as an integer number
    - taskSpeed - the speed at which the astronaut is completing each task as a positive, nonzero integer number

--- OOP - Java 1/HW05/BlueAstronaut.java
+++ OOP - Java 1/HW05/BlueAstronaut.java
@@ -1,4 +1,9 @@
 public class BlueAstronaut extends Player implements Crewmate {
+  // number of tasks that needs to be completed as an integer number
+  private int numTasks; 
+  // speed at which the astronaut is completing each task as a positive, nonzero integer number
+  private int taskSpeed;
+
 
 
 }
