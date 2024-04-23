97a6f31f9dea45a5accc0c0caa55f965907bb2f9
Daniel Ahn <iamdanahn@gmail.com>
2024-04-22 20:33:17 -0400

    A constructor that takes in the name, susLevel, numTasks, and taskSpeed and sets all fields accordingly. It must accept the variables in the specified order.
    A constructor that takes in just a name and assigns the following default values: susLevel 15, numTasks 6, taskSpeed 10

--- OOP - Java 1/HW05/BlueAstronaut.java
+++ OOP - Java 1/HW05/BlueAstronaut.java
@@ -4,6 +4,15 @@ public class BlueAstronaut extends Player implements Crewmate {
   // speed at which the astronaut is completing each task as a positive, nonzero integer number
   private int taskSpeed;
 
+  public BlueAstronaut(String name, int susLevel, int numTasks, int taskSpeed) {
+    super(name, susLevel);
+    this.numTasks = numTasks;
+    this.taskSpeed = taskSpeed;
+  }
+  
+  public BlueAstronaut(String name) {
+    this(name, 15, 6, 10);
+  }
 
 
 }
