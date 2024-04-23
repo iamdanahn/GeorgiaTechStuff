98e1b8345ab3fc9279a6ff7f602f0d92d25abdbf
Daniel Ahn <iamdanahn@gmail.com>
2024-04-22 20:56:03 -0400

    perform actions

--- OOP - Java 1/HW05/Gameplay.java
+++ OOP - Java 1/HW05/Gameplay.java
@@ -10,6 +10,45 @@ public class Gameplay {
    RedAstronaut a2 = new RedAstronaut("Suspicious Person", 100, "expert");
   
 
-   a
+   // 1. Have RedAstronaut Liam sabotage BlueAstronaut Bob. After the sabotage:
+   a1.sabotage(b1);
+
+   // 2. Have RedAstronaut Liam freeze RedAstronaut Suspicious Person:
+   a1.freeze(a2);
+
+   // 3. Have RedAstronaut Liam freeze BlueAstronaut Albert. After the freeze:
+   a1.freeze(b3);
+
+   // 4. Have BlueAstronaut Albert call an emergency meeting:
+   b3.emergencyMeeting();
+
+   // 5. Have RedAstronaut Suspicious Person call an emergency meeting:
+   a2.emergencyMeeting();
+   
+   // 6. Have BlueAstronaut Bob call an emergency meeting:
+   b1.emergencyMeeting();
+
+   // 7. Have BlueAstronaut Heath complete tasks:
+   b2.completeTask();
+
+   // 8. Have BlueAstronaut Heath complete tasks:
+   b2.completeTask();
+
+   // 9. Have BlueAstronaut Heath complete tasks:
+   b2.completeTask();
+
+   // 10. Have RedAstronaut Liam freeze Angel:
+   a1.freeze(b4);
+
+   // 11. Have RedAstronaut Liam sabotage Bob twice:
+   a1.sabotage(b1);
+   a1.sabotage(b1);
+
+   // 12. Have RedAstronaut Liam freeze Bob:
+   a1.freeze(b1);
+
+   // 13. Have BlueAstronaut Angel call emergency meeting:
+   b4.emergencyMeeting();
+
   }
 }
