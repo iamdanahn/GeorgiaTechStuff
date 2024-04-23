64ef4fbde58772b49c24df13821f3d102ee4c26c
Daniel Ahn <iamdanahn@gmail.com>
2024-04-22 20:50:30 -0400

    Create starting players

---
+++ OOP - Java 1/HW05/Gameplay.java
@@ -0,0 +1,13 @@
+public class Gameplay {
+
+  public static void main(String[] args) {
+   BlueAstronaut b1 =  new BlueAstronaut("Bob", 20, 6, 30);
+   BlueAstronaut b2 =  new BlueAstronaut("Heath", 30, 3, 21);
+   BlueAstronaut b3 =  new BlueAstronaut("Albert", 44, 2, 0);
+   BlueAstronaut b4 =  new BlueAstronaut("Angel", 0, 1, 0);
+
+   RedAstronaut a1 = new RedAstronaut("Liam", 19, "experienced");
+   RedAstronaut a1 = new RedAstronaut("Suspicious Person", 100, "expert");
+  
+  }
+}
