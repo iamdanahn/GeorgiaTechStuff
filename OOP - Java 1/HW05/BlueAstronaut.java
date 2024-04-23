5615c70302d3c727de8246f75f96f8c0699b2e80
Daniel Ahn <iamdanahn@gmail.com>
2024-04-22 20:40:25 -0400

    - A BlueAstronaut that is frozen cannot complete tasks. - If taskSpeed is greater than 20, subtract 2 from numTasks. Otherwise, subtract 1 from numTasks. - If numTasks falls below 0, set it to 0
    - After BlueAstronaut is done with their tasks, meaning numTasks is equal to 0 (only for the first time),
    - Print out “I have completed all my tasks”
    - Then reduce BlueAstronaut’s susLevel by 50% (round down)
    - Does not return anything.

--- OOP - Java 1/HW05/BlueAstronaut.java
+++ OOP - Java 1/HW05/BlueAstronaut.java
@@ -14,5 +14,49 @@ public class BlueAstronaut extends Player implements Crewmate {
     this(name, 15, 6, 10);
   }
 
+  public void emergencyMeeting() {
+    if (this.isFrozen()) {
+      return;
+    }
 
+    Player[] players = getPlayers();
+
+    // find player with highest sus level to freeze them if they aren't frozen
+    // already
+    int highestSusLevel = 0;
+    Player sussestPlayer;
+    for (Player p : players) {
+      if (!p.isFrozen()) {
+        if (p.getSusLevel() > highestSusLevel && this != p) {
+          sussestPlayer = p;
+        } else if (p.getSusLevel() == highestSusLevel) {
+          return;
+        }
+      }
+    }
+
+    sussestPlayer.setFrozen(true);
+
+    gameOver();
+  }
+
+  public void completeTask() {
+    if (this.isFrozen()) return;
+    int originalNumTasks = this.numTasks;
+
+    if (this.taskSpeed > 20) {
+      this.numTasks -= 2;
+    } else {
+      this.numTasks--;
+    }
+
+    if (this.numTasks < 0) {
+      this.numTasks = 0;
+    }
+
+    if (this.numTasks == 0 && originalNumTasks > 0) {
+      System.out.println("I have complete all my tasks");
+      this.setSusLevel( (int)this.getSusLevel() * 0.5 );
+    }
+  }
 }
