c7c83a2f5d4994d14bd95283645b6260bb0a4755
Daniel Ahn <iamdanahn@gmail.com>
2024-04-26 22:32:07 -0400

    Variables: - double droolRate
    Constructors:
    - Dog(String name, double health, int painLevel, double droolRate)
    droolRate - If droolRate is less than or equal to zero, set drool rate to 0.5
    - Dog(String name, double health, int painLevel)
    Default droolRate is 5.0

--- OOP - Java 1/HW06/Dog.java
+++ OOP - Java 1/HW06/Dog.java
@@ -1,3 +1,22 @@
 public class Dog extends Pet {
+  private double droolRate;
 
+  public Dog(String name, double health, int painLevel, double droolRate) {
+    super(name, health, painLevel);
+    setDroolRate(droolRate);
+  }
+  public Dog(String name, double health, int painLevel) {
+    this(name, health, painLevel, 5.0);
+  }
+
+  public double getDroolRate() {
+    return this.droolRate;
+  }
+  public void setDroolRate(double droolRate) {
+    this.droolRate = droolRate;
+
+    if (droolRate <= 0) {
+      this.droolRate = 0.5;
+    }
+  }
 }
