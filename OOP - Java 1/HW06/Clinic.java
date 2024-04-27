b57667b9f68e26747bc18cd90383bd3884afe972
Daniel Ahn <iamdanahn@gmail.com>
2024-04-26 23:42:14 -0400

    Constructor 1: Clinic(File file) File contains patient info - assign to patientFile It contains: Name, Type of pet (includes pet info), Appointment info (timeIn, health, painLevel, timeOut), - Day initialized to 1 will add details later once file format is confirmed
    Constructor 2:
    Clinic(String fileName)
    - String includes filename extension – don't add “.csv”
    - Chains to the other constructor

--- OOP - Java 1/HW06/Clinic.java
+++ OOP - Java 1/HW06/Clinic.java
@@ -6,8 +6,17 @@ import java.io.PrintWriter;
 
 public class Clinic() {
   // This is a class representing the vet clinic.
-  
+
   private File patientFile;
   private int day;
 
+  public Clinic(File file) {
+    patientFile = new File(file);
+
+  }
+  public Clinic(String fileName) {
+    String inputFile = fileName.substring(0, fileName.length() - 4);
+    File fileIn = new File(inputFile);
+    this(fileIn);
+  }
 }
