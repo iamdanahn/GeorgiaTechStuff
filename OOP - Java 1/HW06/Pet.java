1c9b4848432d67a560cb868a3888d957ed8e45ec
Daniel Ahn <iamdanahn@gmail.com>
2024-04-26 22:16:00 -0400

    health - If health passed in is greater than 1.0, set health to 1.0 - If health passed in is less than 0.0, set health to 0.0
    painLevel
    - If painLevel passed in is greater than 10, set pain level to 10
    - If painLevel passed in is less than 1, set pain level to 1

--- OOP - Java 1/HW06/Pet.java
+++ OOP - Java 1/HW06/Pet.java
@@ -5,5 +5,25 @@ public class Pet {
   private double health; // percentage ranging from 0.0 to 1.0
   private int painLevel; // 1 to 10
 
-  
+  public Pet(String name, double health, int painLevel) {
+    this.name = name;
+    setHealth(health);
+    setPainLevel(painLevel);
+  }
+
+  public void setHealth(double health) {
+    if (health > 1.0) {
+      this.health = 1.0;
+    } else if (health < 0.0) {
+      this.health = 0.0;
+    }
+  }
+  public void setPainLevel(double painLevel) {
+    if (painLevel > 10) {
+      this.painLevel = 10;
+    } else if (painLevel < 1) {
+      this.painLevel = 1;
+    }
+  }
+
 }
