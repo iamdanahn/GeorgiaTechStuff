bd9e15454424bb1d57caa8b21ce53e068b13bc09
Daniel Ahn <iamdanahn@gmail.com>
2024-04-26 23:02:48 -0400

    Constructor 1: Cat(String name, double health, int painLevel, int miceCaught)
    - If miceCaught passed in is less than 0, set miceCaught to 0

--- OOP - Java 1/HW06/Cat.java
+++ OOP - Java 1/HW06/Cat.java
@@ -1,3 +1,18 @@
 public class Cat extends Pet {
-  
+  private int miceCaught;
+
+  public Cat(String name, double health, int painLevel, int miceCaught) {
+    super(name, health, painLevel);
+    setMiceCaught(miceCaught);
+  }
+
+  public int getMiceCaught() {
+    return this.miceCaught;
+  }
+  public void setMiceCaught(int miceCaught) {
+    this.miceCaught = miceCaught;
+    if (miceCaught < 0) {
+      this.miceCaught = 0;
+    }
+  }
 }
