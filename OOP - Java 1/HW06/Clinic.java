6b83d74af3acb4553c071ed1482a597debcd0478
Daniel Ahn <iamdanahn@gmail.com>
2024-04-26 23:21:11 -0400

    This is a class representing the vet clinic.
    Variables
    - File patientFile = File with patient information
    - int day

---
+++ OOP - Java 1/HW06/Clinic.java
@@ -0,0 +1,13 @@
+import java.io.File;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Clinic() {
+  // This is a class representing the vet clinic.
+  
+  private File patientFile;
+  private int day;
+
+}
