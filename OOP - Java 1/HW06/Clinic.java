e1d046ff6c4b3ddeb82b255bf2f453ea6667c57d
Daniel Ahn <iamdanahn@gmail.com>
2024-04-27 00:57:27 -0400

    Read files that contain name, type of pet, and time of appointments for the day.
    Use scanner to take user input
    
    If typeOfPet is invalid, throw InvalidPetException. Don't catch the code, caller of method should handle the exception

--- OOP - Java 1/HW06/Clinic.java
+++ OOP - Java 1/HW06/Clinic.java
@@ -11,7 +11,8 @@ public class Clinic() {
   private int day;
 
   public Clinic(File file) {
-    patientFile = new File(file);
+    patientFile = file;
+    Scanner fileScan = null;
 
   }
   public Clinic(String fileName) {
@@ -19,4 +20,35 @@ public class Clinic() {
     File fileIn = new File(inputFile);
     this(fileIn);
   }
+
+  public String nextDay(File f) throws FileNotFoundException {
+    File fileIn = new File(f);
+    Scanner scan = null;
+    String[] info = new String[4];
+
+    scan = new Scanner(fileIn);
+    int index = 0;
+    while (scan.hasNext()) {
+      String part = scan.next(",")
+    }
+  }
+
+  public String nextDay(String fileName) throws FileNotFoundException {
+  }
+
 }
+
+// Patients.csv file
+// [Name],[Species],[DroolRate/MiceCaught],[Day],[EntryTime],[ExitTime],[InitialHealth],[InitialPainLevel]
+// Freckles,Cat,5,Day 2,1243,1249,0.68,4 
+// Spots,Dog,6.6,Day 1,1713,1727,0.7,10,Day 3,1240,1245,0.8,4 
+// Susan,Cat,2,Day 3,1403,1515,0.11,4 
+// Dolf,Dog,0.1,Day 1,1523,1539,0.9,7 
+// Margie,Cat,8,Day 2,1653,1656,0.6,3
+
+// Appointments.csv file
+// Pillow,Cat,5,1839 
+// Atlas,Cat,2,1742 
+// Cocoa,Dog,1.4,1630 
+// Starbs,Cat,10,1240 
+// Tucker,Dog,7.9,0918
