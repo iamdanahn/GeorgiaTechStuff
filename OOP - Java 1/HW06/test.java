95729a47dceecf02b97a03f0d357822367075354
Daniel Ahn <iamdanahn@gmail.com>
2024-04-27 12:36:40 -0400

    test page

---
+++ OOP - Java 1/HW06/test.java
@@ -0,0 +1,8 @@
+public class test {
+
+  public static void main(String[] args) {
+    String fileName = "Patients.csv";
+    System.out.println(fileName.substring(0, fileName.length() - 4));
+  
+  }
+}
