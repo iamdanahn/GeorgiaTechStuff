f0481edac2ffa574f9fadce1d88e9302dbd92581
Daniel Ahn <iamdanahn@gmail.com>
2024-04-26 22:09:22 -0400

    Create vars

---
+++ OOP - Java 1/HW06/Pet.java
@@ -0,0 +1,9 @@
+public class Pet {
+// This class represents any pet that would seek consultation from the clinic.
+
+  private String name;
+  private double health; // percentage ranging from 0.0 to 1.0
+  private int painLevel; // 1 to 10
+
+  
+}
