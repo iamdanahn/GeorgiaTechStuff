397ba0e734e355c6f9e25dd196c8e73b20cb0482
Daniel Ahn <iamdanahn@gmail.com>
2024-05-02 22:53:04 -0400

    - addAtIndex(T data, int index) - Adds a node to the position specified by index - If the index is negative or larger than the size of the list, throw IllegalArgumentException with a message “Your index is out of the list bounds” - If the passed data is null, throw IllegalArgumentException with a message “You cannot add null data to the list” - Adjust for head, tail, and size variables accordingly

--- OOP - Java 1/HW07/LinkedList.java
+++ OOP - Java 1/HW07/LinkedList.java
@@ -18,6 +18,34 @@ public class Linkedlist implements List<T> {
     return this.tail;
   }
 
+  public void addAtIndex(T data, int index) {
+    if (index < 0 || isEmpty()) {
+      throw IllegalArgumentException("Your index is out of the list bounds");
+    } else if (data == null) {
+      throw IllegalArgumentException("You cannot add null data to the list");
+    }
+    Node<T> nodeToAdd = new Node<T>(data);
+
+    Node<T> current = head;
+    Node<T> prev = null;
+    int i = 0;
+    while (i != index) {
+      prev = current;
+      current = current.next;
+      i++;
+    }
+    
+    prev.next = nodeToAdd;
+    nodeToAdd.next = current;
+    if (index == 0) {
+      head = nodeToAdd;
+    }
+    if (index == size()) {
+      tail = nodeToAdd;
+    }
+    size++;
+  }
+
   public boolean isEmpty() {
     return size() == 0;
   }
