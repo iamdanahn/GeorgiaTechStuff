e07b7a5abe358d676f44ca23d0aff3295511cbca
Daniel Ahn <iamdanahn@gmail.com>
2024-05-02 21:53:50 -0400

    variables and no args constructor

--- OOP - Java 1/HW07/LinkedList.java
+++ OOP - Java 1/HW07/LinkedList.java
@@ -1,5 +1,14 @@
 import java.util.List;
 
 public class Linkedlist implements List<T> {
+  private Node<T> head;
+  private Node<T> tail;
+  private int size;
 
+  public LinkedList() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  
 }
